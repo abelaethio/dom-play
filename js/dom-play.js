babdfaac5e751ad63098590a77686db136514742
abelaethio <114033068+abelaethio@users.noreply.github.com>
2023-07-20 20:26:19 -0700

    Update dom-play.js

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,55 +1,54 @@
-//alert("Hi from DOM play!");
-
-
+//alert("Hi from dom play");
 /*
 
-    Create an app that on click actor lines are highlighted
+    Creat an app that on click actpors lines are highlighted 
 
-    With a single click, highlight (bold or otherwise) all lines for a specific actor in a play
+    With a single click, highlight (bold or otherwise) all lines for a specific actor in a play 
     Un-highlight (normal or otherwise) all lines from all other actors in the play
-    
-    The function(s) created to do the work must accommodate any number of actors, and any number of additional lines added to the page
-   
-    HTML "hooks" may be added to the page for proper operations
+
+    The function(s) created to do the work must accommodate any number of actors and any number of additional lines added to the page.
+
+    HTML "hooks" may be added to the page for proper operations 
 
 */
 
-//document.querySelector(".hamlet").style.backgroundColor = "yellow";
+//document.querySelector(".Hamlet").style.backgroundColor = " Yellow"
 
-function highlight(el){
+function highlight (el){
 
     if(el.style.backgroundColor == 'white'){//orange
-        el.style.backgroundColor='orange';
+        el.style.backgroundColor= 'orange';
     }else{//white
-        el.style.backgroundColor='white';
-    }
+        el.style.backgroundColor= 'white';
+    }    
+   
+ }
 
-}
 
 function myAlert(){
-    alert("I'm clicked!");
-}
+    alert("I'm Clicked! ");
+ }
 
 function highlightActor(actor){
-    //alert("Hi, I'm " + actor);
-    for(const mySpan of spans){
-        if(actor == mySpan.dataset.actor){//current actor
-            mySpan.style.backgroundColor='orange';
-        }else{//other actor
-            mySpan.style.backgroundColor='white';
-        }
-    }
+    //alert("Hi i am "+ actor);
+    for(const mySpan of spans)
+
+    {if(actor == mySpan.dataset.actor ){//orange
+        mySpan.style.backgroundColor= 'orange';
+    }else{//white
+        mySpan.style.backgroundColor= 'white';
+    }  }
 }
 
-const spans = document.querySelectorAll("#play span");
-console.log(spans);
+ const spans = document.querySelectorAll("#play span");
+ console.log(spans);
 
-for(const mySpan of spans){
-    //mySpan.addEventListener("click",myAlert);
+
+ for(const mySpan of spans){
+   // mySpan.addEventListener("click",myAlert);
     mySpan.addEventListener("click",function(ev){
         highlightActor(mySpan.dataset.actor);
     });
     //alert(mySpan.dataset.actor);
-}
-
+ }
 
